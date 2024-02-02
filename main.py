3779c996be2dac1fd1f99526dac6158f4eab11e3
smorf1koo <gepetrov@edu.hse.ru>
2024-02-02 16:54:57 +0300

    Game score visualisation and new style

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 import pygame as g
 import random
 
@@ -8,7 +7,7 @@ class Food:
     def __init__(self):
         self.x = random.randint(0, 790)
         self.y = random.randint(0, 590)
-        self.color = (0, 0, 255)
+        self.color = (255, 222, 0)
 
     def show(self, screen):
         g.draw.rect(screen, self.color, (self.x, self.y, 10, 10))
@@ -25,7 +24,7 @@ class Snake:
         self.body = [{'x': x, 'y': y}]
         self.width = 10
         self.height = 10
-        self.color = (255, 0, 0)
+        self.color = (132, 233, 0)
         self.length = 0
 
     def show(self, screen):
@@ -93,10 +92,15 @@ def check_collision(snake, food):
     food_y = food.get_y()
     if snake_x < food_x + 10 and snake_x + 10 > food_x \
             and snake_y < food_y + 10 and snake_y + 10 > food_y:
-        # snake.length += 1
-        # snake.grow()
         return True
     return False
+
+
+def draw_text(screen, message, color, x, y, text_size):
+    font = g.font.Font(None, text_size)
+    text = font.render(message, True, color)
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
 
 
 def main():
@@ -113,6 +117,7 @@ def main():
     food = init_food()
     direction_x = direction_y = 0
     g.time.delay(1000)
+    score = 1
 
     while not game_over:
         clock.tick(fps)
@@ -136,22 +141,27 @@ def main():
                     direction_x = 5
                     direction_y = 0
 
-        screen.fill((0, 0, 0))
+        screen.fill((88, 2, 109))  # purple
         show_food(screen, food)
         show_snake(screen, snake)
         move_snake(snake, direction_x, direction_y, growing)
 
         if check_game_over(screen, snake):
             game_over = True
+            draw_text(screen, f'Игра окончена, ваш счет: {score}', (255, 144, 0),
+                      screen.get_width()/2, screen.get_height()/2, 50)
 
         if check_collision(snake, food):
             food = init_food()
             snake.grow()
-            growing = True
+            # growing = True
+            score += 1
 
+        draw_text(screen, f'Ваш счет: {score}', (255, 144, 0),
+                  90, 20, 40)
         g.display.update()
 
-    time.sleep(2)
+    time.sleep(4)
     g.quit()
     quit()
 
